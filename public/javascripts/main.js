3809a50fe307ef30eaa6aab83e8824bc6cbe0db5
Noski <hante_hunter@163.com>
2016-10-02 01:09:23 +0800

    update: more bootstrap components

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,9 +1,6 @@
 $(document).ready(function () {
 
-  // fix table head
-  $('.fixedHeadTableWrapper').scroll(function () {
-    $('.fixedHeadTableWrapper thead').attr('position', 'fixed');
-  });
+  
 
 });
 
