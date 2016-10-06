11957b156d91fe1ac9ac1255fbb7688ccbcc52ad
gulai <294711823@qq.com>
2016-10-06 20:33:10 +0800

    add nameTobpf.py

---
+++ public/py/nameTobpf.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+import json
+
+fin = open('../../settings.json', 'r')
+all_file = json.load(fin)
+fin.close()
+
+
+bpflist = all_file['bpf']
+# print bpflist
+
+bpflist_len = len(bpflist)
+
+
+def nametobpf(name):
+    """
+      将过滤器名转换为相应的pbf表达式
+    """
+
+    bpf = "-1"
+    for i in range(0, bpflist_len):
+        # print i
+        # print bpflist[i]["name"]
+        # print bpflist[i]["value"]
+
+        if name == bpflist[i]["name"]:
+            bpf = bpflist[i]["value"]
+
+    return bpf
+
+
+#name = "yanran"
+#test = nametobpf(name)
+# print test
