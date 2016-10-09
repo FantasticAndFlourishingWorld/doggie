257d54b78484fa2405125e0624ebe7056d454bd2
Noski <hante_hunter@163.com>
2016-10-10 01:42:19 +0800

    finish first chart

--- public/javascripts/flow.js
+++ public/javascripts/flow.js
@@ -4,8 +4,9 @@ var db = new sqlite3.Database(__dirname + '/../../database/packet.db');
 
 $(document).ready(function () {
 
-  var flowChart = echarts.init(document.getElementById('flowChart'));
-  var pktData = [];
+  var lenChart = echarts.init(document.getElementById('lenChart'));
+  var pktLenData = [];
+  var pktNumData = [];
 
   db.all('SELECT * FROM PACKET ORDER BY STIME ASC', function (err, rows) {
     if (rows.length === 0) {
@@ -14,103 +15,166 @@ $(document).ready(function () {
     }
     var lastSec = new Date(rows[0].STIME).getUTCSeconds();
     var len = 0;
+    var pacNum = 0;
     rows.forEach(function (row) {
       var curSec = new Date(row.STIME).getUTCSeconds();
       if (lastSec === curSec) {
         len += row.PLEN;
+        pacNum += 1;
       } else {
-        pktData.push({
-          time: moment(new Date(row.STIME)).format('GGGG.MM.D H:mm:ss'),
+        var time = moment(new Date(row.STIME)).format('GGGG.MM.D H:mm:ss');
+        pktLenData.push({
+          time: time,
           value: len
         });
+        pktNumData.push({
+          time: time,
+          value: pacNum
+        });
         len = 0;
+        pacNum = 0;
         lastSec = curSec;
       }
     });
     if (len > 0) {
-      pktData.push({
+      pktLenData.push({
         time: moment(new Date(rows[rows.length - 1].STIME)).format('GGGG.MM.D H:mm:ss'),
         value: len
       });
     }
 
-    option = {
-      title: {
-        left: 'center',
-        text: '流量'
+    lenOption = {
+      title : {
+        text: '数据流量及数据包数量统计',
+        x: 'center',
+        align: 'right'
+      },
+      grid: {
+        bottom: 80
       },
       toolbox: {
         feature: {
+          restore: {},
           saveAsImage: {}
         }
       },
-      tooltip: {
+      tooltip : {
         trigger: 'axis',
         axisPointer: {
           animation: true
         },
         position: function (pt) {
-          return [pt[0], '20%'];
+          return [pt[0], '40%'];
         }
       },
-      xAxis: {
-        type: 'category',
-        boundaryGap: false,
-        data: pktData.map(function (pkt) {
-          return pkt.time;
-        }),
-        splitLine: {
-          show: false
-        }
+      legend: {
+        data:['流量','数据包数'],
+        x: 'left'
       },
-      yAxis: {
-        type: 'value',
-        boundaryGap: [0, '100%'],
-        splitLine: {
-          show: false
+      dataZoom: [{
+        type: 'inside',
+        start: 0,
+        end: 10
+      }, {
+        start: 0,
+        end: 10,
+        handleIcon: 'M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z',
+        handleSize: '80%',
+        handleStyle: {
+          color: '#fff',
+          shadowBlur: 3,
+          shadowColor: 'rgba(0, 0, 0, 0.6)',
+          shadowOffsetX: 2,
+          shadowOffsetY: 2
         }
-      },
-      dataZoom: [
+      }],
+      xAxis : [
         {
-          show: true,
-          realtime: true,
-          start: 65,
-          end: 85
-        },
-        {
-          type: 'inside',
-          realtime: true,
-          start: 65,
-          end: 85
+          type : 'category',
+          boundaryGap : false,
+          data: pktNumData.map(function (pkt) {
+            return pkt.time;
+          }),
+          splitLine: {
+            show: false
+          }
         }
       ],
-      series: [{
-        name: '流量(byte)',
-        type: 'line',
-        smooth: true,
-        symbol: 'none',
-        sampling: 'average',
-        itemStyle: {
-          normal: {
-            color: 'rgb(255, 70, 131)'
+      yAxis: [
+        {
+          name: '流量',
+          type: 'value',
+          nameLocation: 'start',
+          boundaryGap: [0, '50%'],
+          splitLine: {
+            show: false
           }
         },
-        areaStyle: {
-          normal: {
-            color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
-              offset: 0,
-              color: 'rgb(255, 158, 68)'
-            }, {
-              offset: 1,
+        {
+          name: '数据包数',
+          nameLocation: 'start',
+          type: 'value',
+          boundaryGap: [0, '80%'],
+          inverse: true,
+          splitLine: {
+            show: false
+          }
+        }
+      ],
+      series: [
+        {
+          name: '流量',
+          type: 'line',
+          smooth: true,
+          symbol: 'none',
+          sampling: 'average',
+          itemStyle: {
+            normal: {
               color: 'rgb(255, 70, 131)'
-            }])
-          }
+            }
+          },
+          areaStyle: {
+            normal: {
+              color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+                offset: 0,
+                color: 'rgb(255, 158, 68)'
+              }, {
+                offset: 1,
+                color: 'rgb(255, 70, 131)'
+              }])
+            }
+          },
+          data: pktLenData
         },
-        data: pktData
-      }]
+        {
+          name: '数据包数',
+          type: 'line',
+          yAxisIndex: 1,
+          smooth: true,
+          symbol: 'none',
+          sampling: 'average',
+          itemStyle: {
+            normal: {
+              color: 'rgb(131, 70, 255)'
+            }
+          },
+          areaStyle: {
+            normal: {
+              color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+                offset: 0,
+                color: 'rgb(68, 158, 255)'
+              }, {
+                offset: 1,
+                color: 'rgb(131, 70, 255)'
+              }])
+            }
+          },
+          data: pktNumData
+        }
+      ]
     };
 
-    flowChart.setOption(option);
+    lenChart.setOption(lenOption);
 
   });
 
