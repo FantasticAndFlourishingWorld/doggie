c311f5f7d897679edbb970c7484c24515c37bfc4
gulai <294711823@qq.com>
2016-10-29 00:01:50 +0800

    modified:   ip2geo.py

--- lib/py/ip2geo.py
+++ lib/py/ip2geo.py
@@ -1,5 +1,5 @@
 import geoip2.database
-# from check_ip import is_network_ip
+import check_ip
 
 reader = geoip2.database.Reader('../../database/GeoLite2-City.mmdb')
 
@@ -16,3 +16,40 @@ def ip2geo(ipstr):
     result['location_longitude'] = response.location.longitude
     result['location_latitude'] = response.location.latitude
     return result
+
+
+def add_geo(kou_result):
+    """
+        add src_longitude, src_latitude, dst_longitude and dst_latitude to ip level
+    """
+    IP_dst = kou_result['result']['IP_dst']
+    IP_src = kou_result['result']['IP_src']
+    
+    kou_result['result']['src_longitude'] = None
+    kou_result['result']['src_latitude'] = None
+    kou_result['result']['dst_longitude'] = None
+    kou_result['result']['dst_latitude'] = None
+
+    IP_dst_isnet = check_ip.is_network_ip(IP_dst)
+    IP_src_isnet = check_ip.is_network_ip(IP_src)
+
+    if IP_src_isnet and IP_dst_isnet:
+        return kou_result
+
+    if IP_dst_isnet:
+        temp = ip2geo(IP_dst)
+        kou_result['result']['dst_latitude'] = temp['location_latitude']
+        kou_result['result']['dst_longitude'] = temp['location_longitude']
+    if IP_src_isnet:
+        temp = ip2geo(IP_src)
+        kou_result['result']['src_latitude'] = temp['location_latitude']
+        kou_result['result']['src_longitude'] = temp['location_longitude']
+
+    return kou_result
+
+
+# b = add_geo(a)
+# print "b['result']['src_longitude']:", b['result']['src_longitude']
+# print "b['result']['src_latitude']:", b['result']['src_latitude']
+# print "b['result']['dst_longitude']:", b['result']['dst_longitude']
+# print "b['result']['dst_latitude']:",b['result']['dst_latitude']
